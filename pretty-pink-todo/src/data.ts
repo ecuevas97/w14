62d54730fa3d05c691787bc25aaa3ac2d7767f9d
ecuevas97 <cuevas.elena97@gmail.com>
2025-07-04 18:51:26 -0700

    first comit

---
+++ pretty-pink-todo/src/data.ts
@@ -0,0 +1,5 @@
+export const tasks = [
+  { id: 1, title: "Buy sparkly pink pens 🖊️", done: false },
+  { id: 2, title: "Water my flower babies 🌸", done: true },
+  { id: 3, title: "Code the cutest to-do app 💻✨", done: false },
+];
